7083a1a06f7064d4a805c8c4be967d56ec15d1d3
vkochano <vladimir.kochanov.g@gmail.com>
2018-05-04 02:52:22 -0400

    concat/assign

---
+++ AST/ASTException.h
@@ -0,0 +1,25 @@
+#include <exception>
+#include <string> 
+
+namespace roe
+{
+    
+    class ASTException : std::exception
+    {
+    public:
+        ASTException(const std::string& text)
+            : text_(text)
+        {
+        }
+        
+        const char* what() const noexcept  override
+        {
+            return text_.c_str();
+        }
+        
+    private:
+        std::string text_;
+    };
+    
+    
+}
