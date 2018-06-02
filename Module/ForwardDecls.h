0aa03c7c09aef0fd73a3c9289b976a226e4206c4
vkochano <vladimir.kochanov.g@gmail.com>
2018-06-02 01:56:05 -0400

    install target & forward declarations

---
+++ Module/ForwardDecls.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace llvm
+{
+  class BasicBlock;
+  class ExecutionEngine;
+  class CallInst;
+  class Value;
+  class LLVMContext;
+  class Type;
+  class Function;
+  class Module;
+  class EngineBuilder;
+  class FunctionType;
+}
+
+namespace roe
+{
+  class IRBuilder;
+}
